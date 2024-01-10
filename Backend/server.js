76af8062e95a3503bdbf230ee12b9430bc8aa231
Janindu <qweasd1256987@gmail.com>
2024-01-10 13:37:12 +0530

    added mvc

---
+++ Backend/server.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+const PORT =process.env.PORT || 5005;
+
+// connect to DB and start server
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+    });
+    })
+    
+    .catch((err) => console.log(err));
