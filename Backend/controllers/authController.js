43753017140c1210f8e6683d6980b63053a10373
meow-4O4 <dharmarathna1220@gmail.com>
2024-01-15 21:08:31 +0530

    implement jsonwebtoken

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,4 +1,42 @@
 const User = require("./../models/userModel");
+const jwt = require("jsonwebtoken");
 
 const catchAsync = require("./../utils/catchAsync");
 const AppError = require("./../utils/appError");
+
+const signToken = (id) => {
+  return (
+    jwt.sign({ id: id }),
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    }
+  );
+};
+
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  res.cookie("jwt", token, cookieOptions);
+
+  res.status(statusCode).json({
+    status: "success",
+    token,
+    data: {
+      user,
+    },
+  });
+};
+
+exports.signUp = catchAsync(async (req, res, next) => {
+  const newUser = await User.create(req.body);
+
+  createSendToken(newUser, 200, res);
+});
