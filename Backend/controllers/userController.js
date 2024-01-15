38845dcf7c3b2b01633b3676da7786d0ae59dace
Janindu <qweasd1256987@gmail.com>
2024-01-15 23:33:48 +0530

    in user control file set a new file location for user model

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,5 +1,5 @@
 const asyncHandler = require("express-async-handler") ;
-const User = require("../models/user-models");
+const User = require("../models/userModel");
 
 
 const registerUser = asyncHandler( async (req, res) => {
